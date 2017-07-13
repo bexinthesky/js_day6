c27c717c0d29b06169924181eda8fb6ff8c65aa3
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-07-12 20:55:40 -0700

    steps 1-8

---
+++ script.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById('btn1');
+  console.log(btn);
+
+var links = document.getElementsByTagName('button');
+  console.log(links);
+
+  var oneLink = document.querySelector('ul li:last-child #btn3');
+    console.log(oneLink);
