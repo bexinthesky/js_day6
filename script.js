6e3ed93029d48852794c27ac653a1db0b7dd84db
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-07-12 20:57:05 -0700

    1-8 with comments

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// btn1 .getElementById
 var btn = document.getElementById('btn1');
   console.log(btn);
 
+// btn2 .getElementsByTagName
 var links = document.getElementsByTagName('button');
   console.log(links);
 
+// btn3 .querySelector
   var oneLink = document.querySelector('ul li:last-child #btn3');
     console.log(oneLink);
